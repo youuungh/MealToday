658a76eee230931f715c43f48c8e20a098c28720
Ninezero <kusepz@gmail.com>
2024-02-02 01:48:23 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mealtoday/adapters/CategoryAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.mealtoday.adapters
+
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.mealtoday.data.Hot
+import com.example.mealtoday.databinding.CategoryContentBinding
+import eightbitlab.com.blurview.RenderEffectBlur
+
+class CategoryAdapter: RecyclerView.Adapter<CategoryAdapter.ItemViewHolder>() {
+
+    private val diffUtil = object : DiffUtil.ItemCallback<Hot>() {
+        override fun areItemsTheSame(oldItem: Hot, newItem: Hot): Boolean {
+            return oldItem.idMeal == newItem.idMeal
+        }
+
+        override fun areContentsTheSame(oldItem: Hot, newItem: Hot): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, diffUtil)
+
+    inner class ItemViewHolder(val binding: CategoryContentBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(CategoryContentBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun getItemCount() = differ.currentList.size
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val data = differ.currentList[position]
+
+        Glide.with(holder.itemView)
+            .load(data.strMealThumb)
+            .into(holder.binding.contentImage)
+
+        holder.apply {
+            holder.binding.blurView.setupWith(binding.root, RenderEffectBlur())
+            holder.binding.contentTitle.text = data.strMeal
+        }
+    }
+}
