a4c7ca8aeb5890b3fc8b8eb98863602de74b82a1
Ninezero <kusepz@gmail.com>
2024-02-07 00:10:16 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mealtoday/utils/SwipeToDelete.kt
@@ -0,0 +1,20 @@
+package com.example.mealtoday.utils
+
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class SwipeToDelete: ItemTouchHelper.SimpleCallback(
+    0,
+    ItemTouchHelper.LEFT) {
+
+    override fun onMove(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        target: RecyclerView.ViewHolder
+    ): Boolean {
+        return false
+    }
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+    }
+}
