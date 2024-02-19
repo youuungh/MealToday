c6026824d8af37679114bcb56db9374e758dd0fc
Ninezero <kusepz@gmail.com>
2024-02-19 23:59:51 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mealtoday/data/DrinkList.kt
@@ -0,0 +1,5 @@
+package com.example.mealtoday.data
+
+data class DrinkList(
+    val drinks: List<Drink>
+)
---
+++ app/src/main/java/com/example/mealtoday/network/CockTailApi.kt
@@ -0,0 +1,12 @@
+package com.example.mealtoday.network
+
+import com.example.mealtoday.data.DrinkList
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CockTailApi {
+
+    @GET("filter.php")
+    suspend fun getDrinks(@Query("c") drinkName: String): Response<DrinkList>
+}
