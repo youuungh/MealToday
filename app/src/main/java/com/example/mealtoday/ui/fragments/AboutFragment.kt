c0c6e715e4d5c976445ce67199faa9c932208c3d
Ninezero <kusepz@gmail.com>
2024-03-12 21:57:24 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mealtoday/ui/fragments/AboutFragment.kt
@@ -0,0 +1,57 @@
+package com.example.mealtoday.ui.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.mealtoday.R
+import com.example.mealtoday.databinding.FragmentAboutBinding
+import com.example.mealtoday.utils.openUrl
+import com.google.android.gms.oss.licenses.OssLicensesActivity
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+
+class AboutFragment : Fragment(R.layout.fragment_about), View.OnClickListener {
+
+    private lateinit var navController: NavController
+    private var _binding: FragmentAboutBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        enterTransition = com.google.android.material.transition.platform.MaterialSharedAxis(
+            com.google.android.material.transition.platform.MaterialSharedAxis.Z,
+            true
+        ).addTarget(view)
+        returnTransition = com.google.android.material.transition.platform.MaterialSharedAxis(
+            com.google.android.material.transition.platform.MaterialSharedAxis.Z,
+            false
+        ).addTarget(view)
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentAboutBinding.bind(view)
+        navController = Navigation.findNavController(view)
+
+        binding.back.setOnClickListener { navController.popBackStack() }
+
+        binding.cvDev.setOnClickListener(this)
+        binding.cvOther.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.cvDev -> openUrl("https://github.com/youuungh")
+            R.id.cvOther -> {
+                OssLicensesMenuActivity.setActivityTitle(getString(R.string.oss_title))
+                startActivity(Intent(requireActivity(), OssLicensesMenuActivity::class.java))
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
